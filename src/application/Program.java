3bee5cc8a7567712a49259277800d2694eff7a24
Henrique Alves Brito <155191957+HenrykeAlmeida@users.noreply.github.com>
2024-11-27 06:36:20 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -31,5 +32,9 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-	}
+		System.out.println("\n===== Test 4: seller insert ====== ");
+		Seller newSeller = new Seller(null, "Henrique Alves", "Greg@3122.gmail", new Date(), 7000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+		}	
 }
