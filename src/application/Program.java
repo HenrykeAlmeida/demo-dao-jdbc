df1d75a705a66c360222d66940c08b7622b8977f
Henrique Alves Brito <155191957+HenrykeAlmeida@users.noreply.github.com>
2024-11-27 02:36:03 -0300

    implements FindById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,17 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "book");
-		
-		Seller seller = new Seller(21, "Henrique", "Henrique1322@gmail", new Date(), 1000.0, obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 	}
